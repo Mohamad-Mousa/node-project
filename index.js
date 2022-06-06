abe3081738600d5192a45bb5b6669f630dcf06c8
Mohamad-Mousa <101283640+Mohamad-Mousa@users.noreply.github.com>
2022-06-07 01:15:15 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 import express from "express";
-import routes from "./src/routes/crmRoutes";
-import mongoose from "mongoose";
+import routes from "./src/ongoRoutes/ongoRoutes";
+import mongoose, { mongo } from "mongoose";
 import bodyParser from 'body-parser';
-
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './src/swagger.json';
 
 const app = express();
 const PORT = 4000;
 
 //mongoose connection
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/CRMdb', {
+mongoose.connect('mongodb://localhost/ongoapp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-//bodyParser setup
-app.use(bodyParser.urlencoded({ extended: true}));
+//bodyparser setup
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
 
 routes(app);
 
-//serving static files
-app.use(express.static('public'));
-
-app.get('/', (req, res)=> 
+app.get('/' , (req, res) => {
     res.send(`Node and express server running on port ${PORT}`)
-);
+});
 
-app.listen(PORT, ()=>
+app.listen(PORT, () => {
+    app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     console.log(`Your server is running on port ${PORT}`)
-);
+});
